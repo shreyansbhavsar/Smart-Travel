3899ad6ecbc3c81e9e40321f6f716751e8e04774
Shreyans Bhavsar <shreyansbhavsar9950@gmail.com>
2021-03-18 15:36:08 +0530

    reset password functionality

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -19,4 +21,8 @@ urlpatterns = [
     path('forgotpassword', views.forgotpassword, name='forgotpassword'),
     path('myorders', views.myorders, name='myorders'),
     path('addphoto/<int:id>', views.addphoto, name='addphoto'),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='password_reset.html'),name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'),name="password_reset_complete"),
 ]
